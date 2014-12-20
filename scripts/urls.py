6491f970647a97d4debb6e166a9694641e140608
scottdnz <scottdnz@hotmail.com>
2014-12-20 18:02:06 +1300

    Initial commit.

---
+++ scripts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from scripts import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^test_form$', views.test_form, name='test_form'),
+    url(r'^file_uploader$', views.file_uploader, name='file_uploader')
+    #url(r'^scriptpile/scripts$', views.index, name='index')
+)
---
+++ scripts/views.py
@@ -0,0 +1,53 @@
+
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.conf import settings
+#from django.http import HttpResponse
+
+# Create your views here.
+
+#def index(request):
+#test_vals = {"val1": "value1"}
+#    return render(request, 'index.html', test_vals)
+#    return HttpResponse("Hello, world. You're at the scripts index.")
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def test_form(request):
+    if request.method == 'GET':
+        return render(request, 'test_form.html')
+    elif request.method == 'POST':
+        resp = {'firstName': '',
+                'category': '',
+                'longText': ''}
+        if request.POST.has_key('firstName'):
+            resp['firstName'] = request.POST.get('firstName')
+        if request.POST.has_key('firstName'):
+            resp['category'] = request.POST.get('category')
+        if request.POST.has_key('longText'):
+            resp['longText'] = request.POST.get('longText')
+        return render_to_response('test_form_confirm.html', resp)
+    
+
+def file_uploader(request):
+    if request.method == 'GET':
+        return render(request, 'file_uploader.html')
+    elif request.method == 'POST':
+        resp = {"result": "good"}
+        if request.FILES.has_key('fileToUpload'):
+            resp['f_name'] = ''
+            try:
+                handle_uploaded_f(request.FILES['fileToUpload'])
+            except Exception as exc:
+                resp["result"] = "bad, " + exc.__str__()    
+        return render_to_response('file_uploader_confirm.html', resp)
+    
+    
+def handle_uploaded_f(f):
+    full_path = settings.MEDIA_ROOT + '/' + f.name
+    with open(full_path, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return
